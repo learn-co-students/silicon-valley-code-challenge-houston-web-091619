e228206982354c4791f98b944c1ec8e333904f26
alexiscash <alexiscash97@rocketmail.com>
2019-09-24 17:56:07 -0500

    done

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,67 @@
 class Startup
 
+    @@all = []
+
+    attr_reader :founder, :domain
+    attr_accessor :name
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+
+        @@all << self
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder)
+        self.all.find do |i|
+            i.founder == founder
+        end
+    end
+
+    def self.domains
+        self.all.map do |i|
+            i.domain
+        end
+    end
+
+    def sign_contract(venture, type, investment)
+        FundingRound.new(self, venture, type, investment)
+    end
+
+    def funding_rounds 
+        FundingRound.all.select do |i|
+            i.startup == self
+        end
+    end
+
+    def num_funding_rounds
+        self.funding_rounds.size
+    end
+
+    def total_funds
+        self.funding_rounds.map(&:investment).reduce(&:+)
+    end
+
+    def investors
+        self.funding_rounds.map do |i|
+            i.venture_capitalist
+        end.uniq
+    end
+
+    def big_investors
+        self.investors.select do |i|
+            VentureCapitalist.tres_commas_club.include?(i)
+        end
+    end
+
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,52 @@
 class VentureCapitalist
 
+    @@all = []
+
+    attr_accessor :name, :total_worth
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        self.all.select do |i|
+            i.total_worth >= 1000000000
+        end
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select do |i|
+            i.venture_capitalist == self
+        end
+    end
+
+    def portfolio
+        self.funding_rounds.map do |i|
+            i.startup
+        end.uniq
+    end
+
+    def biggest_investment
+        self.funding_rounds.max_by do |i|
+            i.investment
+        end
+    end
+
+    def invested(domain)
+        self.funding_rounds.select do |i|
+            i.startup.domain == domain
+        end.map(&:investment).reduce(&:+)
+    end
+    
 end
